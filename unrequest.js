a3689e2190e3b09761a2ea88ce8b396a57eab117
gilad <ggochman@gmail.com>
2020-10-18 22:30:43 +0300

    compatable with instagram policy

--- unrequest.js
+++ unrequest.js
@@ -6,21 +6,20 @@ const dotenv =require('dotenv');
 dotenv.config({path: "./config.env"});
 
 async function start(usr, pass) {
-    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
-    var bitches = [];
+    //if list is available:
+    //`first\nsecond\n`.split('\n')
+//   
+    var bitches = [ ];
+
 
     await page.goto('https://www.instagram.com/');
 
     //LOGIN
-    await page.waitForSelector('div.-MzZI:nth-child(1) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)');
-    await page.type('div.-MzZI:nth-child(1) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)', usr);
-
-    await page.waitForSelector('div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)');
-    await page.type('div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)', pass);
-
-    await page.waitForSelector('.L3NKy > div:nth-child(1)');
-    await page.click('.L3NKy > div:nth-child(1)');
+    await login();
+    
+    if (bitches.length<1){
     
     // goto settings
     await page.waitForSelector('span.qNELH > img:nth-child(1)');
@@ -74,41 +73,62 @@ async function start(usr, pass) {
         console.log("Reached: ",bitchesShown);
 
     }
-    await page.waitFor(5000);
-    for (let index = 0; index < bitches.length; index++) {
-        const element = bitches[index];
-        const page3 = await browser.newPage();
-        await page3.goto('https://www.instagram.com/'+element);
-        // confirm it is requested
-        let body = await page3.evaluate(() => document.body.innerHTML);
-        const $ = cheerio.load(body);
-        
-        try {
-            await page3.waitForSelector('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button');
-        let button = $('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button',body);
-        let label = button.text();
-        console.log(label);
-        if (label === "Requested"){
-// click unfollow
-        // await page3.waitForSelector('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button');
-        await page3.click('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button');
-        
-        // if pops click unfollow
-            await page3.waitForSelector('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_');
-            await page3.click('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_');    
-        }
-
-        } catch (error) {
-            console.error(error);
-        }
-       
-        
-         await page3.waitFor(3000);
-         await page3.close();
+        await page.waitForTimeout(5000);
     }
     
+
+    await unrequestAll();
+    await browser.close();
     
 
+
+    async function login() {
+        await page.waitForSelector('div.-MzZI:nth-child(1) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)');
+        await page.type('div.-MzZI:nth-child(1) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)', usr);
+
+        await page.waitForSelector('div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)');
+        await page.type('div.-MzZI:nth-child(2) > div:nth-child(1) > label:nth-child(1) > input:nth-child(2)', pass);
+
+        await page.waitForSelector('.L3NKy > div:nth-child(1)');
+        await page.click('.L3NKy > div:nth-child(1)');
+    }
+
+    async function unrequestAll() {
+        for (let index = 0; index < bitches.length; index++) {
+            const element = bitches[index];
+            const page3 = await browser.newPage();
+            await page3.goto('https://www.instagram.com/' + element);
+            // confirm it is requested
+            let body = await page3.evaluate(() => document.body.innerHTML);
+            const $ = cheerio.load(body);
+
+            try {
+                await page3.waitForSelector('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button');
+                let button = $('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button', body);
+                let label = button.text();
+                console.log(element ,': ' ,label);
+                if (label === "Requested") {
+                    // click unfollow
+                    // await page3.waitForSelector('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button');
+                    // await page.waitForTimeout(1000);
+                    await page3.click('#react-root > section > main > div > header > section > div.nZSzR > div.Igw0E.IwRSH.eGOV_.ybXk5._4EzTm > div > div > button');
+
+                    // if pops click unfollow
+                    await page3.waitForSelector('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_');
+                    // await page.waitForTimeout(1500);
+                    await page3.click('body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.-Cab_');
+                }
+
+            } catch (error) {
+                console.error(error);
+            }
+            
+            //instagram only allows 1 request cancel in a minute, after 15 cancels...
+            if (index<15) await page3.waitForTimeout(2500);
+            else await page3.waitForTimeout(60000);
+            await page3.close();
+        }
+    }
 }
 start(process.env.NAME, process.env.PASS);
 
